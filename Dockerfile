6d9559b5857e697d6d3ed248dde16808d389c6c5
IS <ivoseeba@gmail.com>
2021-03-30 20:40:12 +0300

    Configured postgres db.

--- Dockerfile
+++ Dockerfile
@@ -2,9 +2,14 @@ FROM python:3.7-alpine
 LABEL MAINTAINER="London App Developer Ltd, Iv S"
 
 ENV PYTHONUNBUFFERED 1
-COPY ./requirements.txt /requirements.txt
 
+COPY ./requirements.txt /requirements.txt
+RUN apk add --update --no-cache postgresql-client jpeg-dev
+RUN apk add --update --no-cache --virtual .tmp-build-deps \
+	gcc libc-dev linux-headers postgresql-dev musl-dev zlib zlib-dev
 RUN pip install -r /requirements.txt
+RUN apk del .tmp-build-deps
+
 
 RUN mkdir /app
 WORKDIR /app
---
+++ app/core/management/commands/wait_for_db.py
@@ -0,0 +1,17 @@
+import time
+
+from django.db import connections
+from django.db.utils import OperationalError
+from django.core.management.base import BaseCommand
+
+
+class Command(BaseCommand):
+    """Django command to pause execution until database is available"""
+
+    def handle(self, *args, **options):
+        db_conn = None
+        while not db_conn:
+            try:
+                db_conn = connections['default']
+            except OperationalError:
+                time.sleep(1)
