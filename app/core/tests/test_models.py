b5a59b289fec9bc018392722f0ae1f1568d4e6f3
IS <ivoseeba@gmail.com>
2021-04-04 10:08:11 +0300

    added recipe model.

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -58,3 +58,15 @@ def test_ingredient_str(self):  # manage.py makemigrations core; .. migrate
     )
 
     self.assertEqual(str(ingredient), ingredient.name)
+
+
+def test_recipe_str(self):
+    """Test the recipe string representation"""
+    recipe = models.Recipe.objects.create(
+        user=sample_user(),
+        title='Steak and mushroom sauce',
+        time_minutes=5,
+        price=5.00
+    )
+
+    self.assertEqual(str(recipe), recipe.title)
